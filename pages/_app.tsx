7e1fe6b2b3024bd745633f613de031a0bf3a2fba
Joaoaalves <joaoaa21@hotmail.com>
2024-07-26 14:55:33 -0300

    Added an pattern for errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { NextAuthProvider } from '@/contexts/AuthProvider';
+
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <NextAuthProvider>
